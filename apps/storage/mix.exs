ee994d999fc83e3c5e272cbec7ea9d7123a653dc
Hentioe_Cl <me@bluerain.io>
2019-02-02 11:52:45 +0800

    Rewrite version

--- apps/storage/mix.exs
+++ apps/storage/mix.exs
@@ -4,7 +4,7 @@ defmodule Storage.MixProject do
   def project do
     [
       app: :storage,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
