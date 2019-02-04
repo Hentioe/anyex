8c531f448caa235cb376bedbfb9fe3140701a70f
Hentioe_Cl <me@bluerain.io>
2019-02-04 15:39:48 +0800

    Add comments to timestamps()

--- apps/storage/lib/storage/migration.ex
+++ apps/storage/lib/storage/migration.ex
@@ -4,8 +4,13 @@ defmodule Storage.Migration do
     quote do
       use Ecto.Migration
 
+      alias Ecto.Migration.Runner
+
       def common_fields(:v001) do
-        timestamps(type: :utc_datetime_usec, comment: "写入时间")
+        opts = Keyword.merge(Runner.repo_config(:migration_timestamps, []), null: false)
+        type = :utc_datetime_usec
+        add :inserted_at, type, Keyword.merge(opts, comment: "插入时间")
+        add :updated_at, type, Keyword.merge(opts, comment: "更新时间")
         add :res_status, :integer, default: 0, null: false, comment: "资源状态"
       end
     end
