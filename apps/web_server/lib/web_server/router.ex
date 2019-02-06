6f4cbbbfcfcb4da1bf54eb5ed1b25616d2c84e48
Hentioe_Cl <me@bluerain.io>
2019-02-06 13:39:02 +0800

    Add POST /[schema]/admin common API

--- apps/web_server/lib/web_server/router.ex
+++ apps/web_server/lib/web_server/router.ex
@@ -1,5 +1,9 @@
 defmodule WebServer.Router do
   @moduledoc false
+  defmacro __using__(schema: schema) do
+    apply(__MODULE__, :schema, [schema])
+  end
+
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
@@ -13,15 +17,22 @@ defmodule WebServer.Router do
     end
   end
 
-  def schema do
+  def schema(schema) do
     quote do
       use Plug.Router
 
       alias WebServer.Plugs.{JSONHeaderPlug, JwtAuthPlug}
 
       plug :match
+
+      plug Plug.Parsers,
+        parsers: [Plug.Parsers.JSON],
+        json_decoder: Jason
+
       plug JSONHeaderPlug
-      plug JwtAuthPlug
+
+      if Mix.env() == :prod, do: plug(JwtAuthPlug)
+
       plug :dispatch
 
       def resp_json(conn, body, status \\ 200) when is_integer(status) do
@@ -29,12 +40,29 @@ defmodule WebServer.Router do
         |> send_resp(status, Jason.encode!(body))
       end
 
-      def resp_error(conn, error, data \\ %{}) do
-        resp_json(conn, %{passed: false, message: to_string(error), data: data})
+      def resp_error(conn, error) when is_map(error) do
+        message = Map.get(error, :message)
+
+        if message == nil do
+          resp_json(conn, %{passed: false, message: "EXPECTED", data: error})
+        else
+          resp_json(conn, %{passed: false, message: message, data: nil})
+        end
+      end
+
+      def resp_error(conn, error, data \\ %{}) when is_binary(error) do
+        resp_json(conn, %{passed: false, message: error, data: data})
       end
 
       def resp_success(conn, data \\ %{}) do
         resp_json(conn, %{passed: true, message: "SUCCESS", data: data})
+      end
+
+      def resp(conn, result) when is_tuple(result) do
+        case result do
+          {:ok, data} -> resp_success(conn, data)
+          {:error, e} -> resp_error(conn, e)
+        end
       end
 
       def fetch_paging_params(conn, limit) do
@@ -42,6 +70,20 @@ defmodule WebServer.Router do
         offset = Map.get(conn.params, "offset", 0)
         limit = Map.get(conn.params, "limit", limit)
         [conn, [offset: offset, limit: limit]]
+      end
+
+      get "/list" do
+        [conn, paging] = fetch_paging_params(var!(conn), 50)
+        filters = Keyword.merge(paging, res_status: 0)
+
+        conn |> var! |> resp(unquote(schema).find_list(filters))
+      end
+
+      get "admin/list" do
+        [conn, paging] = fetch_paging_params(var!(conn), 50)
+        filters = paging
+
+        conn |> var! |> resp(unquote(schema).find_list(filters))
       end
     end
   end
--- apps/web_server/lib/web_server/routes/article_router.ex
+++ apps/web_server/lib/web_server/routes/article_router.ex
@@ -1,27 +1,13 @@
 defmodule WebServer.Routes.ArticleRouter do
   @moduledoc false
-  use WebServer.Router, :schema
-
   alias Storage.Schema.{Article}
 
-  get "/list" do
-    [conn, paging] = fetch_paging_params(conn, 50)
-    filters = Keyword.merge(paging, res_status: 0)
+  use WebServer.Router, schema: Article
 
-    case Article.find_list(filters) do
-      {:ok, list} -> resp_success(conn, list)
-      {:error, e} -> resp_error(conn, e)
-    end
-  end
-
-  get "admin/list" do
-    [conn, paging] = fetch_paging_params(conn, 50)
-    filters = paging
-
-    case Article.find_list(filters) do
-      {:ok, list} -> resp_json(conn, list)
-      {:error, e} -> resp_error(conn, e)
-    end
+  post "/admin" do
+    article = conn.body_params
+    result = article |> Article.add()
+    conn |> resp(result)
   end
 
   use WebServer.RouterHelper, :default_routes
