f2662458355bef3851fff3d93caa01ed9bf91bdc
Hentioe_Cl <me@bluerain.io>
2019-02-14 06:13:01 +0800

    Ready to release 0.3.0

--- apps/web_server/mix.exs
+++ apps/web_server/mix.exs
@@ -4,7 +4,7 @@ defmodule WebServer.MixProject do
   def project do
     [
       app: :web_server,
-      version: "0.2.1",
+      version: "0.3.0-dev",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
