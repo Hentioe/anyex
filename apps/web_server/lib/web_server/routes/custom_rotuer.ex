f89ee71a81b00631310ccf141e05a19d901c2d70
Hentioe_Cl <me@bluerain.io>
2019-02-19 02:37:12 +0800

    Add custom includes params

--- apps/web_server/lib/web_server/routes/custom_rotuer.ex
+++ apps/web_server/lib/web_server/routes/custom_rotuer.ex
@@ -2,7 +2,32 @@ defmodule WebServer.Routes.CustomRouter do
   @moduledoc false
   use WebServer.Router, :json_support
 
+  # alias Storage.Schema.{
+  #   ArticleRouter,
+  #   CategoryRouter,
+  #   TagsRouter,
+  #   CommentRouter,
+  #   TweetRouter,
+  #   LinkRouter
+  # }
+
   get "/" do
+    conn = conn |> fetch_query_params()
+    includes = conn.params["includes"]
+    includes = includes || ""
+
+    includes
+    |> String.split(",")
+    |> Enum.map(fn include ->
+      try do
+        {:ok, String.to_existing_atom(include)}
+      rescue
+        _e in _ -> {:error, "nnknown resource type: #{include}"}
+      end
+    end)
+    |> Enum.filter(&(elem(&1, 0) == :ok))
+    |> Enum.map(&elem(&1, 1))
+
     conn |> resp_success([])
   end
 
